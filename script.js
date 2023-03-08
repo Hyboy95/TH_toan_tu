738f414842472b8b1c36294e48c8648f746eabc9
hyboy95 <temilator@gmail.com>
2023-03-08 14:12:44 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+inputWidth = prompt('Nhập vào chiều rộng hình chữ nhật: ');
+inputHeight = prompt('Nhập vào chiều cao hình chữ nhật: ');
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write('Diện tích hình chữ nhật = ' + area);
+console.log(area);
